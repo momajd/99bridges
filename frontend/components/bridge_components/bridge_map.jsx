fe4f095adecbe177a4cac2e5cc4b5a96c594ab13
momajd <mo.a.majd@gmail.com>
2016-07-06 17:30:28 -0400

    markers on map

--- frontend/components/bridge_components/bridge_map.jsx
+++ frontend/components/bridge_components/bridge_map.jsx
@@ -1,5 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var BridgeStore = require('../../stores/bridge_store');
+var ClientActions = require('../../actions/client_actions');
 
 var BridgeMap = React.createClass({
   componentDidMount: function() {
@@ -9,6 +11,24 @@ var BridgeMap = React.createClass({
       zoom: 13
     };
     this.map = new google.maps.Map(mapDOMNode, mapOptions);
+
+    BridgeStore.addListener(this.placeMarkers);
+    ClientActions.fetchAllBridges();
+  },
+
+  placeMarkers: function () {
+    var bridgeKeys = Object.keys(this.props.bridges);
+
+    bridgeKeys.forEach(function(key) {
+      var lat = this.props.bridges[key].lat;
+      var lng = this.props.bridges[key].long;
+      var markerPos = {lat: lat, lng: lng};
+
+      var marker = new google.maps.Marker({
+        position: markerPos,
+        map: this.map
+      });
+    }, this);
   },
 
   render: function () {
