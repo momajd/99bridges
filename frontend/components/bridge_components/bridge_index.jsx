5f9877a15a93110057cd1a4e09964e4186530f7f
momajd <mo.a.majd@gmail.com>
2016-07-06 15:35:31 -0400

    google map

--- frontend/components/bridge_components/bridge_index.jsx
+++ frontend/components/bridge_components/bridge_index.jsx
@@ -4,19 +4,7 @@ var ClientActions = require('../../actions/client_actions');
 var BridgeIndexItem = require('./bridge_index_item');
 
 var BridgeIndex = React.createClass({
-  getInitialState: function() {
-    return {bridges: BridgeStore.all()};
-  },
-
-  updateBridges: function() {
-    this.setState({bridges: BridgeStore.all()});
-  },
-
-  componentDidMount: function() {
-    BridgeStore.addListener(this.updateBridges);
-    ClientActions.fetchAllBridges();
-  },
-
+  
   render: function() {
     var bridgeKeys = Object.keys(this.props.bridges);
     var bridges = this.props.bridges;
--- frontend/components/bridge_components/bridge_map.jsx
+++ frontend/components/bridge_components/bridge_map.jsx
@@ -1,9 +1,19 @@
 var React = require('react');
+var ReactDOM = require('react-dom');
 
 var BridgeMap = React.createClass({
+  componentDidMount: function() {
+    var mapDOMNode = ReactDOM.findDOMNode(this.refs.map);
+    var mapOptions = {
+      center: {lat: 37.7758, lng: -122.435}, // this is SF
+      zoom: 13
+    };
+    this.map = new google.maps.Map(mapDOMNode, mapOptions);
+  },
+
   render: function () {
     return (
-      <div className='map' ref='map'>I'm the map!</div>
+      <div className='map' ref='map'></div>
     );
   }
 });
