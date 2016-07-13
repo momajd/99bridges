4eafc47fa3a46a41b1c6684476ba0f70cfee2b4a
momajd <mo.a.majd@gmail.com>
2016-07-13 11:55:35 -0400

    add panorama to bridge show page

--- frontend/components/bridge_components/show_map.jsx
+++ frontend/components/bridge_components/show_map.jsx
@@ -8,17 +8,40 @@ var ShowMap = React.createClass({
 
   placeMap: function () {
     var bridge = this.props.bridge;
+    var latLng = new google.maps.LatLng(bridge.lat, bridge.lng);
     var mapDOMNode = document.getElementById('show-page-map');
     var mapOptions = {
-      center: {lat: bridge.lat, lng: bridge.lng},
+      center: latLng,
       zoom: 14,
     };
     this.map = new google.maps.Map(mapDOMNode, mapOptions);
+    this.placeMarker(latLng);
+    this.placePanorama(latLng);
+    window.map = this.map;
+  },
+
+  placeMarker: function (latLng) {
+    var marker = new google.maps.Marker({
+      position: latLng,
+      map: this.map,
+      icon: {
+        url: 'http://maps.google.com/mapfiles/marker_purple.png'
+      },
+    });
+  },
+
+  placePanorama: function (latLng) {
+    var mapDOMNode = document.getElementById('show-page-pano');
+    var panoOptions = {position: latLng}
+    var pano = new google.maps.StreetViewPanorama(mapDOMNode, panoOptions);
+    this.map.setStreetView(pano);
   },
 
   render: function() {
     return (
-      <div className='show-page-map' id='show-page-map'>
+      <div>
+        <div className='show-page-map' id='show-page-map'></div>
+        <div className='show-page-pano' id='show-page-pano'></div>
       </div>
     );
   }
