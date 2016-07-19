7f0480929067a1dd6fb6cdbd4dfb2688b85e9a69
momajd <mo.a.majd@gmail.com>
2016-07-19 10:17:30 -0400

    user page uses user store instead of session

--- frontend/components/user_components/user_bridges.jsx
+++ frontend/components/user_components/user_bridges.jsx
@@ -5,18 +5,21 @@ var Panel = require('react-bootstrap').Panel;
 var UserBridges = React.createClass({
 
   render: function() {
-    var rows = this.props.user.bridges.map(function(bridge) {
-      var link = '#/bridges/' + bridge.id;
-      return (
-        <tr key={bridge.id}>
-          <td>{bridge.id}</td>
-          <td>{bridge.title}</td>
-          <td>{bridge.description}</td>
-          <td>TODO</td>
-          <td><a href={link}>Link</a></td>
-        </tr>
-      );
-    });
+    var rows;
+    if (this.props.user.bridges) {
+      rows = this.props.user.bridges.map(function(bridge) {
+        var link = '#/bridges/' + bridge.id;
+        return (
+          <tr key={bridge.id}>
+            <td>{bridge.id}</td>
+            <td>{bridge.title}</td>
+            <td>{bridge.description}</td>
+            <td>TODO</td>
+            <td><a href={link}>Link</a></td>
+          </tr>
+        );
+      });
+    }
 
     return (
         <Panel collapsible defaultExpanded header="Created Bridges">
--- frontend/components/user_components/user_show.jsx
+++ frontend/components/user_components/user_show.jsx
@@ -1,14 +1,32 @@
 var React = require('react');
-var SessionStore = require('../../stores/session_store');
 var UserBridges = require('./user_bridges');
+var ClientActions = require('../../actions/client_actions');
+var UserStore = require('../../stores/user_store');
 var PageHeader = require('react-bootstrap').PageHeader;
 var ListGroup = require('react-bootstrap').ListGroup;
 var ListGroupItem = require('react-bootstrap').ListGroupItem;
 
 var UserShow = React.createClass({
 
+  getInitialState: function() {
+    return { user: {} };
+  },
+
+  updateUser: function() {
+    this.setState({user: UserStore.user()});
+  },
+
+  componentDidMount: function() {
+    ClientActions.fetchUser(this.props.params.userId);
+    this.listener = UserStore.addListener(this.updateUser);
+  },
+
+  componentWillUnmount: function() {
+    this.listener.remove();
+  },
+
   render: function() {
-    var user = SessionStore.currentUser();
+    var user = this.state.user;
     return (
       <div className='user-show-page'>
         <PageHeader>
